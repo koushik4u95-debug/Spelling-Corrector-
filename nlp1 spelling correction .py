b965ba0be979d474f28f8c543acc0c3186c15ee1
koushik4u95-debug <70466481+koushik4u95-debug@users.noreply.github.com>
2021-01-18 14:30:29 +0530

    Update nlp1 spelling correction .py

--- nlp1 spelling correction .py
+++ nlp1 spelling correction .py
@@ -1,8 +1,3 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[1]:
-
 
 from textblob import TextBlob
 words = ["rndom ", "Learnin"]
@@ -14,46 +9,3 @@ print("Corrected Words are :")
 for i in corrected_words:
     print(i.correct(), end=" ")
     
-
-
-# In[ ]:
-
-
-
-
-
-# In[ ]:
-
-
-
-
-
-# In[ ]:
-
-
-
-
-
-# In[ ]:
-
-
-
-
-
-# In[ ]:
-
-
-
-
-
-# In[ ]:
-
-
-
-
-
-# In[ ]:
-
-
-
-
